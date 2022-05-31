b6cdd05bb146ace62ad3d9893a5e517b94b0709c
Crefi <alex.a702@yahoo.com>
2022-05-31 19:33:21 +0300

    initial commit

---
+++ App.js
@@ -0,0 +1,18 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet,  View } from 'react-native';
+import RockPaper from './componets/RockPaper';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <RockPaper/>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
